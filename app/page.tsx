e71fc15bcea4b385d98466987db034700bada205
Erfan Jahanbakhsh <erfan.jahanbakhsh82@gmail.com>
2025-09-16 12:24:38 +0330

    used linking and navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+
 export default function page() {
-  return <h1>hello world !</h1>
+  return (
+    <>
+      <h1>hello world !</h1>
+      <Link href="/users">Users</Link>
+      {/* or you can use <a> */}
+    </>
+  )
 }
